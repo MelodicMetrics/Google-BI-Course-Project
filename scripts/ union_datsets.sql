7b9ccfa35a8cfcb2e070aed909e3ac0db5c389a3
MelodicMetrics <milesdavidson@msn.com>
2024-10-21 14:05:45 -0400

    Add SQL query for union datasets

---
+++ scripts/ union_datsets.sql
@@ -0,0 +1,11 @@
+SELECT * 
+FROM 
+  iconic-fountain-435918-q3.Google_Fiber.Market_1
+UNION ALL
+SELECT *
+FROM
+  iconic-fountain-435918-q3.Google_Fiber.Market_2
+UNION ALL
+SELECT * 
+FROM
+  iconic-fountain-435918-q3.Google_Fiber.Market_3
